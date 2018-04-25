a48cc706c9578cb2970dd80f008c7e116a4cbde4
westbooks <1348997701@qq.com>
2018-04-25 10:33:03 +0800

    修改 发送邮件内容

--- fakeSCDN/send_mail.py
+++ fakeSCDN/send_mail.py
@@ -7,7 +7,7 @@ os.environ['DJANGO_SETTINGS_MODULE'] = 'testDjango.settings'
 
 if __name__ == '__main__':
 
-    subject, from_email, to = '来自www.liujiangblog.com的测试邮件', 'skyysn@sina.com', '1348997701@qq.com'
+    subject, from_email, to = '来自施小峰的测试邮件', 'skyysn@sina.com', '1348997701@qq.com'
     text_content = '欢迎访问www.liujiangblog.com，这里是刘江的博客和教程站点，专注于Python和Django技术的分享！'
     html_content = '<p>欢迎访问<a href="http://www.liujiangblog.com" target=blank>www.liujiangblog.com</a>，这里是刘江的博客和教程站点，专注于Python和Django技术的分享！</p>'
     msg = EmailMultiAlternatives(subject, text_content, from_email, [to])
